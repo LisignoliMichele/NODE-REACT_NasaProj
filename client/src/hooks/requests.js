e6ce2d496f1cc6ffb30edcda1f7d86895f4aa38f
Michele Lisignoli <michele@lisignoli.com>
2021-08-23 23:31:19 +0200

    launches model - GET / launches

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -7,9 +7,13 @@ async function httpGetPlanets() {
 }
 
 
+// Load launches, sort by flight number, and return as JSON.
 async function httpGetLaunches() {
-  // TODO: Once API is ready.
-  // Load launches, sort by flight number, and return as JSON.
+  const response = await fetch(`${API_URL}/lauches`);
+  const fetchedLaunches = await response.json();
+  return fetchedLaunches.sort((a, b) =>{
+    return a.flightNumber - b.flightNymber
+  })
 }
 
 async function httpSubmitLaunch(launch) {
