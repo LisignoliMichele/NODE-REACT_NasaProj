da24c7e4a9f5230805755d379b6fba9734e41e93
Michele Lisignoli <michele@lisignoli.com>
2021-08-24 12:01:24 +0200

    POST / launches

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,11 +1,11 @@
 const launches = new Map();
-
+let latestFlightNumber = 100;
 const launch = {
-   flightNumber: 103,
+   flightNumber: 100,
    mission: 'new heart exploration',
    rocket: 'Amerigo Vespucci',
-   lauchDate: new Date('December 3, 2030'),
-   destination: 'Kepler-442 b',
+   launchDate: new Date('December 3, 2030'),
+   target: 'Kepler-442 b',
    customer: ['NASA', 'ZMT'],
    upcoming: true,
    success: true,
@@ -13,6 +13,24 @@ const launch = {
 
 launches.set(launch.flightNumber, launch);
 
+function getAllLaunches() {
+   return Array.from(launches.values());
+}
+
+function addNewLaunch(launch){
+   latestFlightNumber++;
+   launches.set(
+      latestFlightNumber,
+      Object.assign(launch, {
+         flightNumber: latestFlightNumber,
+         customer: ['NASA', 'MICHELE'],
+         upcoming: true,
+         success: true,
+      })
+   )
+};
+
 module.exports = {
-   launches,
+   getAllLaunches,
+   addNewLaunch
 };
